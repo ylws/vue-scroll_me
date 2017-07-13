be955c06db932a982ab61b89776ee10ceaccdf68
ylws <474569696@qq.com>
2017-07-13 19:48:48 +0800

    updage

--- scroll_me/index.js
+++ scroll_me/index.js
@@ -1,6 +1,6 @@
-import {ShineonScroll} from './src/Plugins_es6_scroll_2.0.js'
+import {CFCMScroll} from './src/Plugins_es6_scroll_2.0.js'
 export default {
  	install: function(Vue, name = '$Scroll') {
-	 Object.defineProperty(Vue.prototype, name, { value: ShineonScroll });
+	 Object.defineProperty(Vue.prototype, name, { value: CFCMScroll });
   }
 }
