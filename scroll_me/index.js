a1a7322674ad8eb704d117f3b708e45b2a21f97a
ylws <474569696@qq.com>
2017-07-19 15:22:20 +0800

    parents fn

--- scroll_me/index.js
+++ scroll_me/index.js
@@ -1,6 +1,6 @@
 import {CFCMScroll} from './src/Plugins_es6_scroll_2.0.js'
 export default {
- 	install: function(Vue, name = '$Scroll') {
-	 Object.defineProperty(Vue.prototype, name, { value: CFCMScroll });
+ 	install: function(Vue) {
+	 Object.defineProperty(Vue.prototype, '$Scroll', { value: CFCMScroll });
   }
 }
